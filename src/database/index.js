cb44ecf47eaaf9eeb46513fa32d29da7f0301eb6
Joe <joaovictorcostasouza15@gmail.com>
2021-01-02 10:30:03 -0300

    Corrigindo variavel row

--- src/database/index.js
+++ src/database/index.js
@@ -10,7 +10,7 @@ const client = new Client({
 
 client.connect();
 
-exports.query = async (query) => {
-  const { row } = await client.query(query);
-  return row;
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values);
+  return rows;
 };
