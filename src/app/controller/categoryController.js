e5f262a7c7fe041d9bf996b86f2f7106c815eeb4
Joe <joaovictorcostasouza15@gmail.com>
2021-01-13 21:39:39 -0300

    Configurando categorias

--- src/app/controller/categoryController.js
+++ src/app/controller/categoryController.js
@@ -1,17 +1,67 @@
-const { request } = require('express');
-
 const repositories = require('../repositories/categoryRepositories');
 
 class CategoriesController {
-  index() {}
+  async index(request, response) {
+    const categories = await repositories.findAll();
 
-  show() {}
+    return response.json(categories);
+  }
 
-  store() {}
+  async store(request, response) {
+    const { name } = request.body;
+    if (!name) {
+      return response.status(400).json({ error: 'Is required' });
+    }
 
-  update() {}
+    const category = await repositories.create({ name });
 
-  delete() {}
+    return response.json(category);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await repositories.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ erro: 'User not found ' });
+    }
+
+    return response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Is required' });
+    }
+
+    const category = await repositories.findById(id);
+
+    if (!category) {
+      return response.json({ error: 'User not found' });
+    }
+
+    const updateCategory = await repositories.update(id, { name });
+
+    return response.status(200).json(updateCategory);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const category = await repositories.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ erro: 'User not found' });
+    }
+
+    await repositories.delete(id);
+
+    return response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoriesController();
